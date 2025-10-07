3be135021c00db3500158c971ddd7b909b02e5e7
DanielFeng <danielf6635@nycstudents.net>
2025-10-07 13:18:54 -0400

    start

---
+++ src/Level.java
@@ -0,0 +1,9 @@
+public class Level
+{
+    /** Returns true if the player reached the goal on this level and returns false otherwise */  public boolean goalReached()
+    { /* implementation not shown */ }
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    { /* implementation not shown */ }
+// There may be instance variables, constructors, and methods that are not shown.
+}
